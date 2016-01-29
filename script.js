17ee0ac11b180fbe9d27e357178fdcb464e0adc9
obduracy <liz.apple3@gmail.com>
2016-01-29 16:01:56 -0500

    added flowtype functionality

--- script.js
+++ script.js
@@ -18,5 +18,45 @@ $(window).load(function(){
 
 })
 
+$('.skills').flowtype({
+   minimum   : 200,
+   maximum   : 1200,
+   minFont   : 9,
+   maxFont   : 55,
+   fontRatio : 70
+});
 
+
+
+$('.secondary-titles').flowtype({
+   minimum   : 100,
+   maximum   : 500,
+   minFont   : 8,
+   maxFont   : 40,
+   fontRatio : 35
+})
+
+$('.secondary-text').flowtype({
+   minimum   : 100,
+   maximum   : 500,
+   minFont   : 7,
+   maxFont   : 14,
+   fontRatio : 30
+})
+
+$('.locations').flowtype({
+   minimum   : 100,
+   maximum   : 500,
+   minFont   : 8,
+   maxFont   : 14,
+   fontRatio : 30
+})
+
+$('.end').flowtype({
+   minimum   : 200,
+   maximum   : 500,
+   minFont   : 10,
+   maxFont   : 14,
+   fontRatio : 30
+})
 
